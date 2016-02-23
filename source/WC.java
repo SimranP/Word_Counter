8a4359d5f003221f678f8a84e74977bd1d185a2d
Simran Pal <simranp@thoughtworks.com>
2016-02-23 14:10:34 +0530

    added basic functions of wc

--- source/WC.java
+++ source/WC.java
@@ -1,5 +1,26 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 class WC {
-  public static int countCharacters(String string){
-    return string.length();
+  private String text;
+
+  public WC(String text){
+    this.text = text;
   };
+
+  public int characters(){
+    return this.text.length();
+  };
+
+  public int words(){
+    if(this.text.isEmpty()) return 0;
+    Pattern p = Pattern.compile("\\s+");
+    return p.split(this.text).length;
+  };
+
+  public int lines(){
+    Pattern p = Pattern.compile("\\n");
+    return p.split(this.text).length-1;
+  };
+
 }
--- test/WCTest.java
+++ test/WCTest.java
@@ -4,17 +4,66 @@ import org.junit.Test;
 public class WCTest {
   @Test
   public void countCharaters_gives_the_number_of_characters_in_piece_of_text(){
-    int chars = WC.countCharacters("apple");
-    assertEquals(chars,5);
+    WC someText = new WC("");
+    assertEquals(someText.characters(),0);
   };
+
+  @Test
+  public void countCharaters_gives_the_number_of_characters_in_piece_of_text1(){
+    WC someText = new WC("apple");
+    assertEquals(someText.characters(),5);
+  };
+
   @Test
   public void countCharaters_gives_the_number_of_characters_in_piece_of_text2(){
-    int chars = WC.countCharacters("apple\nmango");
-    assertEquals(chars,11);
+    WC someText = new WC("apple\nmango");
+    assertEquals(someText.characters(),11);
   };
+
   @Test
   public void countCharaters_gives_the_number_of_characters_in_piece_of_text3(){
-    int chars = WC.countCharacters("apple\n\rmango");
-    assertEquals(chars,12);
+    WC someText = new WC("apple\n\rmango");
+    assertEquals(someText.characters(),12);
+  };
+
+  @Test
+  public void countWords_gives_the_number_of_words_in_piece_of_text(){
+    WC someText = new WC("");
+    assertEquals(someText.words(),0);
+  };
+
+  @Test
+  public void countWords_gives_the_number_of_words_in_piece_of_text1(){
+    WC someText = new WC("apple is a red fruit");
+    assertEquals(someText.words(),5);
+  };
+
+  @Test
+  public void countWords_gives_the_number_of_words_in_piece_of_text2(){
+    WC someText = new WC(" ");
+    assertEquals(someText.words(),0);
+  };
+
+  @Test
+  public void countWords_gives_the_number_of_words_in_piece_of_text3(){
+    WC someText = new WC("\n\r");
+    assertEquals(someText.words(),0);
+  };
+
+  @Test
+  public void countLines_gives_the_number_of_words_in_piece_of_text(){
+    WC someText = new WC("\n\r");
+    assertEquals(someText.lines(),1);
+  };
+  @Test
+  public void countLines_gives_the_number_of_words_in_piece_of_text2(){
+    WC someText = new WC("apple\n\ris\n\rsmall");
+    assertEquals(someText.lines(),2);
+  };
+
+  @Test
+  public void countLines_gives_the_number_of_words_in_piece_of_text3(){
+    WC someText = new WC("apple\nis\nsmall");
+    assertEquals(someText.lines(),2);
   };
 }
